30b5fe230f8c70f810cc4ee595d7071114cfa78d
Peter <gkfnrkfl@naver.com>
2020-01-11 18:28:37 +0900

    RxFlow 기반 rootVC 변경

--- RxFlowTest/SceneDelegate.swift
+++ RxFlowTest/SceneDelegate.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
+import RxFlow
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    var coordinator = FlowCoordinator()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -19,9 +22,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = scene as? UIWindowScene else { return }
         self.window = UIWindow(windowScene: windowScene)
-        window?.backgroundColor = .white
-        window?.makeKeyAndVisible()
-        window?.rootViewController = SearchViewController()
+        
+        let appFlow = AppFlow()
+        
+        Flows.whenReady(flow1: appFlow) {
+            self.window?.backgroundColor = .white
+            self.window?.makeKeyAndVisible()
+            self.window?.rootViewController = $0
+        }
+        
+        self.coordinator.coordinate(flow: appFlow, with: AppStepper())
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
